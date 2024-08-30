5500da8c063ca8993824d4a10e987381e7bcb545
sdwilli0 <sdwilli0@our.ecu.edu.au>
2024-08-30 14:04:26 +0800

    Latest again

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from gui.main_window import MainWindow
 from PyQt6.QtWidgets import QApplication
 import sys
@@ -7,14 +6,4 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-=======
-from gui.main_window import MainWindow
-from PyQt6.QtWidgets import QApplication
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
->>>>>>> 3508a699cb3b3084ed8ee2b733643c54a21b8047
     sys.exit(app.exec())
