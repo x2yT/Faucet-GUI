a5348d29547db725618a499241afd5fb8939638e
sdwilli0 <sdwilli0@our.ecu.edu.au>
2024-09-03 10:03:12 +0800

    latest

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from gui.main_window import MainWindow
 from PyQt6.QtWidgets import QApplication
 import sys
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
