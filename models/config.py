99ca42ab1118959c317508e90ad8a53731a46fe1
muditc932 <muditc@our.ecu.edu.au>
2024-09-20 19:09:44 +0530

    Update config.py

--- models/config.py
+++ models/config.py
@@ -1,6 +1,7 @@
 class Config:
-    def __init__(self, vlans, routers, dps, acls):
+    def __init__(self, vlans, routers, dps, acls, meters):
         self.vlans = vlans
         self.routers = routers
         self.dps = dps
         self.acls = acls
+        self.meters = meters
