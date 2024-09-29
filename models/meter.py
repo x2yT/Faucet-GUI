3c4e251bd3b09bc268487dce8ce26ce1a1a0151f
muditc932 <muditc@our.ecu.edu.au>
2024-09-30 02:22:22 +0800

    updated bands and flags as dict

--- models/meter.py
+++ models/meter.py
@@ -1,34 +1,31 @@
-# models/meter.py
-
 class Meter:
-    def __init__(self, meter_id, bands=None, unit="kbps"):
+    def __init__(self, meter_id, name, rate, burst_size, flags=None, bands=None):
         self.meter_id = meter_id
-        self.bands = bands or []
-        self.unit = unit
-
-    @classmethod
-    def from_dict(cls, data):
-        meter_id = data.get('meter_id', None)
-        bands = data.get('bands', [])
-        unit = data.get('unit', "kbps")
-        return cls(meter_id=meter_id, bands=bands, unit=unit)
-
-    def to_dict(self):
-        return {
-            'meter_id': self.meter_id,
-            'bands': self.bands,
-            'unit': self.unit
-        }
-
-class Band:
-    def __init__(self, rate, burst_size=None, action=None):
+        self.name = name
         self.rate = rate
         self.burst_size = burst_size
-        self.action = action
+        self.flags = flags if flags else []
+        self.bands = bands if bands else []
 
     def to_dict(self):
+        """Converts the meter object to a dictionary suitable for saving to YAML."""
         return {
+            'meter_id': self.meter_id,
+            'entry': {
+                'flags': self.flags,
+                'bands': self.bands
+            },
             'rate': self.rate,
-            'burst_size': self.burst_size,
-            'action': self.action
+            'burst_size': self.burst_size
         }
+
+    @staticmethod
+    def from_dict(name, data):
+        """Creates a Meter object from a dictionary loaded from YAML."""
+        meter_id = data.get('meter_id', 0)
+        rate = data['rate']
+        burst_size = data.get('burst_size', 0)
+        flags = data['entry'].get('flags', [])
+        bands = data['entry'].get('bands', [])
+
+        return Meter(meter_id, name, rate, burst_size, flags, bands)
